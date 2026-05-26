09f1e3371c5af3551e61a4e72f362b4e6f065a6c
Praveen Kumar <praveen@rashwinindustries.com>
2026-05-26 17:24:51 +0530

    fix: ensure app.js and .htaccess are copied to dist

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-import('./dist/server/server.js').catch(console.error);
+import fs from 'fs';
+
+const serverPath = fs.existsSync('./server/server.js') 
+    ? './server/server.js' 
+    : './dist/server/server.js';
+
+import(serverPath).catch(console.error);
